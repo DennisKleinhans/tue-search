14b4b80bd114e9854d68164e8cfd0d5086e3f827
Dennis <Dennis.Kleinhans991@gmx.de>
2024-06-14 18:06:31 +0200

    add pagination

--- frontend/src/components/SearchResults.js
+++ frontend/src/components/SearchResults.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import ResultItem from "./ResultItem";
+import Pagination from "./Pagination";
 
 const SearchResults = ({ results }) => {
+  const resultsPerPage = 10;
+  const [currentPage, setCurrentPage] = useState(1);
+
+  const totalPages = Math.ceil(results.length / resultsPerPage);
+
+  const showPagination = results.length > 0 && results.length > resultsPerPage;
+
+  const startIndex = (currentPage - 1) * resultsPerPage;
+  const endIndex = startIndex + resultsPerPage;
+  const currentResults = results.slice(startIndex, endIndex);
+
+  // Navigationsfunktionen
+  const goToPage = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   return (
     <div>
-      {results.map((result) => (
+      {currentResults.map((result) => (
         <ResultItem key={result.id} result={result} />
       ))}
+
+      {showPagination && (
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={goToPage}
+        />
+      )}
     </div>
   );
 };
