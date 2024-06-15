d0ed469c24f83bee8c61ec4a29c2ed44ba7f2268
Dennis <Dennis.Kleinhans991@gmx.de>
2024-06-15 11:18:53 +0200

    reorder view of ResultItem component

--- frontend/src/components/ResultItem.js
+++ frontend/src/components/ResultItem.js
@@ -4,10 +4,13 @@ const ResultItem = ({ result }) => {
   return (
     <div
       style={{
-        margin: "20px 0",
-        padding: "10px",
+        margin: "10px 0",
+        padding: "4px",
       }}
     >
+      <div style={{ color: "#006621", fontSize: "14px", margin: "5px 0" }}>
+        {result.url}
+      </div>
       <a
         href={result.url}
         target="_blank"
@@ -23,9 +26,6 @@ const ResultItem = ({ result }) => {
       >
         {result.title}
       </a>
-      <div style={{ color: "#006621", fontSize: "14px", margin: "5px 0" }}>
-        {result.url}
-      </div>
       <p style={{ color: "#545454", fontSize: "16px", margin: "5px 0" }}>
         {result.description}
       </p>
