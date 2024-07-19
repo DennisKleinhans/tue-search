0fb7b5ca665db15f1f8e19674e7f3c891f6057a5
Karyl <44136068+KarylReyne@users.noreply.github.com>
2024-07-19 19:04:44 +0200

    edits+testing for tuesday

--- src/retrieval_system/logistic_regression/executable.py
+++ src/retrieval_system/logistic_regression/executable.py
@@ -1,10 +1,14 @@
 import sys
 import os
+from nltk.corpus import stopwords
+from nltk.stem import WordNetLemmatizer
 
 
 if __name__ == "__main__":
     sys.path.insert(0, f"{os.getcwd()}")
     from src.retrieval_system.logistic_regression.retrieval_interface import RetrievalSystemInterface
+    from src.retrieval_system.logistic_regression.preprocessing import preprocess
 
     RSI = RetrievalSystemInterface()
     RSI.train_retrieval_system()
+    
